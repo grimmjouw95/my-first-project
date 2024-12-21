b57fda29c3dd39fb5e056af4046b7782c05ca5d7
grimmjouw95 <bataev.a95@mail.ru>
2024-12-21 14:54:50 +0300

    Add files via upload

---
+++ my first project on hyperskill.py
@@ -0,0 +1,11 @@
+print("""Earned amount:
+Bubblegum: $202
+Toffee: $118
+Ice cream: $2250
+Milk chocolate: $1680
+Doughnut: $1075
+Pancake: $80""")
+print("Income: $", float(202+118+2250+1680+1075+80))
+staff_expenses = int(input('Staff expenses:'))
+other_expenses = int(input('Other expenses:'))
+print( 'Net income: $', 5405 - (staff_expenses + other_expenses))
